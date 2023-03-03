1739e369b0e9e560ec1241ba39021d4a6bc97ae7
Matthew Lee <mdlee1998@gmail.com>
2023-03-02 21:31:12 -0500

    :card_file_box: JSON server database with temp data

--- frontend/src/components/InitiativeBody/InitiativeBody.jsx
+++ frontend/src/components/InitiativeBody/InitiativeBody.jsx
@@ -1,33 +1,16 @@
-import InitiativeBodyRow from "../InitiativeBodyRow/InitiativeBodyRow";
+import { useState, useEffect } from "react";
 
+import InitiativeBodyRow from "../InitiativeBodyRow/InitiativeBodyRow";
 import "./InitiativeBody.scss";
 
 const InitiativeBody = () => {
-  const encounterData = [
-    { id: 1, initiative: 15, name: 'Ailane', armor_class: 28, hit_points: 6 },
-    {
-      id: 2,
-      initiative: 4,
-      name: 'Realcube',
-      armor_class: 14,
-      hit_points: 489
-    },
-    {
-      id: 3,
-      initiative: 8,
-      name: 'Jetpulse',
-      armor_class: 24,
-      hit_points: 105
-    },
-    {
-      id: 4,
-      initiative: 14,
-      name: 'Wordware',
-      armor_class: 20,
-      hit_points: 252
-    },
-    { id: 5, initiative: 15, name: 'Quaxo', armor_class: 29, hit_points: 48 }
-  ];
+  const [turns, setTurns] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/turns')
+      .then(res => res.json())
+      .then(data => setTurns(data));
+  }, []);
 
   return ( 
     <div className="initiative-table p-0.5 text-2xl">
@@ -38,7 +21,7 @@ const InitiativeBody = () => {
         <h3 className="w-1/6 m-0.5 text-center relative">HP</h3>
       </div>
       <div className="initiative-body flex flex-col">
-          {encounterData.map(creature => <InitiativeBodyRow key={creature.id}>{creature}</InitiativeBodyRow>)}
+          {turns && turns.map(creature => <InitiativeBodyRow key={creature.id}>{creature}</InitiativeBodyRow>)}
       </div>
     </div>
    );
